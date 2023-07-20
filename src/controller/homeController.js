b6d73e6db992d9e495cb094a5b809eb61a9e0ff2
NguyenDai2001 <dai242001@gmail.com>
2023-07-21 01:46:32 +0700

    2

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,3 +1,13 @@
+import mysql from 'mysql2';
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'fullstack'
+});
+
+
+
+
 const handleHelloWorld = (req, res) => {
     const name = "Nguyen dai";
     return res.render('home.ejs', { name })
@@ -6,7 +16,30 @@ const homeUserPage = (req, res) => {
     return res.render('user.ejs')
 }
 
+const handleCreateNewUser = (req, res) => {
+    console.log("checK: ", req.body)
+    let email = req.body.emailName;
+    let password = req.body.passwordName;
+    let username = req.body.userName;
+
+
+    connection.query(
+        'INSERT INTO users(email,password,username) VALUES(?,?,?)', [email, password, username],
+        function (err, results, fields) {
+            console.log("check 1", results); // results contains rows returned by server
+            console.log("check 2", fields); // fields contains extra meta data about results, if available
+        }
+    );
+
+
+    return res.send("aaaaaaaa")
+}
+
+
+
+
 module.exports = {
     handleHelloWorld,
-    homeUserPage
+    homeUserPage,
+    handleCreateNewUser
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -8,6 +8,9 @@ const router = express.Router();
 const initWebRoutes = (app) => {
     router.get("/", homeController.handleHelloWorld)
     router.get("/user", homeController.homeUserPage)
+    router.post('/user/create-user', homeController.handleCreateNewUser)
+
+
     return app.use("/", router);
 }
 export default initWebRoutes;
